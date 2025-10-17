1d6a40d02d3b93f6a3873e589f2eb4ab643ee390
unknown <okqwaszx12@naver.com>
2025-10-17 18:05:06 +0900

    first

---
+++ MVC 2강 전반부/src/main/java/hello/thymleafbasic/ThymleafbasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thymleafbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymleafbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymleafbasicApplication.class, args);
+	}
+
+}

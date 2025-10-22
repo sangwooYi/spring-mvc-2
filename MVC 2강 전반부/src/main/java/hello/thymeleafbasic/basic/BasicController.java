bcee25c7634e885c10257d88c8344a939f0bed18
unknown <okqwaszx12@naver.com>
2025-10-22 18:06:01 +0900

    aa

--- MVC 2강 전반부/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ MVC 2강 전반부/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -119,6 +119,52 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        list.add(new User("userD", 110));
+        model.addAttribute("users", list);
+    }
+
+
     // @ComponentScan 이 @Component 를 전부 스캔해서 자동 빈 등록해줌
     @Component("helloBean")
     static class HelloBean {

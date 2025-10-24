2a84dad7d25bc49592e4c47c00cf0ce53c082021
unknown <okqwaszx12@naver.com>
2025-10-24 18:03:20 +0900

    aa

--- MVC 2강 전반부/src/main/java/hello/thymeleafbasic/basic/TemplateController.java
+++ MVC 2강 전반부/src/main/java/hello/thymeleafbasic/basic/TemplateController.java
@@ -1,9 +1,11 @@
 package hello.thymeleafbasic.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/template")
 public class TemplateController {
@@ -11,5 +13,15 @@ public class TemplateController {
     @GetMapping("/fragment")
     public String template() {
         return "template/fragment/fragmentMain";
+    }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtends() {
+        return "template/layoutExtend/layoutFile";
     }
 }
